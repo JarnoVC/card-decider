2f3169772f571b86bce98fbe1bb2a80a80392a1d
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-09-02 19:37:21 +0200

    Feat: added a bit of responsiveness

--- src/components/MainCardList.vue
+++ src/components/MainCardList.vue
@@ -29,6 +29,39 @@
 }
 
 
+
+  /* Responsive adjustments */
+  @media (max-width: 2000px) {
+    .card-display {
+      grid-template-columns: repeat(4, 1fr); /* 4 columns for medium screens */
+      gap: 100px;
+      padding: 15px;
+    }
+  }
+
+  @media (max-width: 992px) {
+    .card-display {
+      grid-template-columns: repeat(3, 1fr); /* 3 columns for smaller screens */
+      gap: 100px;
+      padding: 10px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .card-display {
+      grid-template-columns: repeat(2, 1fr); /* 2 columns for tablets */
+      gap: 100px;
+      padding: 5px;
+    }
+  }
+
+  @media (max-width: 576px) {
+    .card-display {
+      grid-template-columns: 1fr; /* 1 column for mobile devices */
+      gap: 100px;
+      padding: 5px;
+    }
+  }
  
 </style>
   
